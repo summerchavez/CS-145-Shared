baf03a875632bf3e41dbc3e9466a803914a1dc49
mathhawk3 <mathhawk3@gmail.com>
2024-01-30 11:06:32 -0800

    Created the add and subtract methods

--- lab_3_letter_inventory/LetterInventory.java
+++ lab_3_letter_inventory/LetterInventory.java
@@ -1,41 +1,42 @@
-//Trevor Figgins
+//Trevor Figgins, Brendan Leonard, Summer Chavez
 //1/29/2024
 //CS 145
-//Pre Lab 3
+//Lab 3 - Letter Inventory
 
-//Mock up of the LetterInventory Lab
+package CS_145_Shared.lab_3_letter_inventory;
+import java.util.Arrays;
 
 public class LetterInventory{
     private int[] counts;
     private int size;
 
-    // public LetterInventory(String data){
-    //     //should have a [a-zA-Z] for acceptable terms to pass through
-    //     //probably another nested for loop to just cycle through per character 
-    //     for(i){
-    //         for(j){
-    //             count+;
-    //         }
-    //     }
-    // }
+    public LetterInventory(String data){
+        //TODO
+    }
+
+    public LetterInventory(int[] data){
+        if(data.length!=26){
+            throw new IllegalArgumentException("Data must have size 26");
+        }
+        this.counts = data.clone();
+        this.size = 0;
+        for(int count : data){
+            size++;
+        }
+    }
 
     //should take in size as the limit for a for loop and then maybe an if statement to get a count on 
     //how many of each char their is in the array
-    // public int get(char letter){
-    //     for(size; i++){
-    //         if(contains char){
-    //         count ++;
-    //         }
-    //         exception thrown if out of bounds
-    //     }
-    // }
+    public int get(char letter){
+        //TODO
+        return(0);//PLACEHOLDER
+    }
 
     //just setting the count of a letter in the list to the passed int value
     //pretty sure its way more complicated than this
-    // public void set(char letter, int value){
-    //     int letterCount = letter
-    //     letter = int value
-    // }
+    public void set(char letter, int value){
+        //TODO
+    }
 
     //counts the size of the inventory/keeps track
     public int size(){
@@ -56,21 +57,31 @@ public class LetterInventory{
                 c += (char)('a'+ i);
             }
         }
+        return(c);
     }
 
     //takes the counts for each letter from two inventory and adds them
     //returns a new inventory count w/o effecting the old lists
-    public LetterInventory add(){
-        //TODO
-        inventory1 + inventory2 = new inventory
-        return new inventory
+    public LetterInventory add(LetterInventory other){
+        int[] result = new int[26];
+        for(int i = 0; i < 26; i++) {
+            result[i] = this.counts[i]+other.counts[i];
+        }
+        return(new LetterInventory(result));
     }
 
     //takes the counts for each letter from two inventory and subtracts leaving the difference
     //returns a new inventory count w/o effecting the old lists
-    public LetterInventory subtract(){
-        //TODO
-        inventory1 - inventory2 = new inventory
-        return new inventory 
+    //If any of the counts would be negative, returns null instead.
+    public LetterInventory subtract(LetterInventory other){
+        int[] result = new int[26];
+        for(int i = 0; i < 26; i++) {
+            if(this.counts[i]<other.counts[i]){
+                return(null);
+            } else {
+                result[i] = this.counts[i] - other.counts[i];
+            }
+        }
+        return(new LetterInventory(result));
     }
 }
